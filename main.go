35f5e1aa985d21ed92b66227be46551a03e67723
Bolaji Adeyemi <d.bola1@gmail.com>
2023-08-06 13:04:30 +0100

    Add home and about pages

--- main.go
+++ main.go
@@ -1,78 +1,40 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 const portNumber = ":8080"
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the home page")
+	renderTemplate(w, "home.page.html")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := addValues(2, 2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+	renderTemplate(w, "about.page.html")
+
 }
 
-// addValues adds two integers and return the sum
-func addValues(x, y int) int {
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
 
-	return x + y
-}
-
-// routine that handles error
-
-func Divide(w http.ResponseWriter, r *http.Request) {
-	var x float32
-	var y float32
-	x = 100.0
-	y = 10.0
-	f, err := divideValues(x, y)
+	err := parsedTemplate.Execute(w, nil)
 
 	if err != nil {
-		fmt.Fprintf(w, "cannot divide by 0")
+		fmt.Println("error pasring template:", err)
 		return
 	}
-
-	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", x, y, f))
-}
-
-func divideValues(x, y float32) (float32, error) {
-
-	if y <= 0 {
-		err := errors.New("cannot divide by zero")
-		return 0, err
-	}
-	result := x / y
-	return result, nil
 }
 
 // main is the main application function
 func main() {
-	// fmt.Println("Hello, world")
-
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	n, err := fmt.Fprintf(w, "Hello, world!")
-
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 	}
-
-	// 	// fmt.Println("Bytes written:" + n)
-
-	// 	fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	// })
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
 
-	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
-	_ = http.ListenAndServe(portNumber, nil) // the first 1024 ports (on any computing systems) are privileged, you have to be a privileged user to use it
+	_ = http.ListenAndServe(portNumber, nil)
 }
-
-//go test -coverprofile=coverage.out && go tool cover -html=coverage.out
